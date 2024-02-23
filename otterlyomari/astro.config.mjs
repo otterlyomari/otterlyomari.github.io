a966574f126ceb7f09a0b20c68de2b03336a02f9
Omari A. Tidemere <161001270+otterlyomari@users.noreply.github.com>
2024-02-23 18:13:53 -0500

    Update astro.config.mjs

--- otterlyomari/astro.config.mjs
+++ otterlyomari/astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://otterlyomari.github.io',
-        base: '/otterlyomari.github.io',
+        base: '/otterlyomari',
 	integrations: [mdx(), sitemap()],
 });
