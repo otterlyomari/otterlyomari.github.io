250a5ddbac3f2ea22a3ccc006a409065e6a6e4a7
Neon-Gr33n <161001270+otterlyomari@users.noreply.github.com>
2024-02-23 22:45:53 -0500

    -progress, slowly, but surely

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import icon from "astro-icon";
+
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://otterlyomari.github.io',
-	integrations: [mdx(), sitemap()],
+  site: 'https://otterlyomari.github.io',
+  integrations: [mdx(), sitemap(), icon({
+    include: {
+      // Include all brand icons
+      bi: ['*']
+    }
+  }), metaTags()]
 });
