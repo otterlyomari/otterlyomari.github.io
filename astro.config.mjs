8d779f18e0172c544264a00a09f801ed180b4c16
neon-gr33n <161001270+otterlyomari@users.noreply.github.com>
2026-05-27 17:59:34 -0400

    Prep to move to Cloudflare Pages
    GitHub pages is suboptimal for my purposes, and as such, this page will be migrated to a different host, this repository will still be the core of it though!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,26 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import icon from "astro-icon";
-import react from "@astrojs/react";
+import icon from 'astro-icon';
+import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://otterlyomari.github.io',
+
+  // IMPORTANT for Cloudflare Pages (static hosting)
+  output: 'static',
+
+  // no base needed unless you're deploying under a subpath
   base: '/',
-  
-  integrations: [mdx(), sitemap(), icon({
-    include: {
-      // Include all brand icons
-      bi: ['*']
-    }
-  }),  react()]
+
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        bi: ['*'],
+      },
+    }),
+    react(),
+  ],
 });
