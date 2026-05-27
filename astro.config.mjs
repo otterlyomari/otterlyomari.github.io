931822c257b59c7bd8f620d8a81ff4dfa3734e92
neon-gr33n <161001270+otterlyomari@users.noreply.github.com>
2026-05-27 00:57:48 -0400

    Somehow broke the fucking imports -w- fixed!

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import icon from "astro-icon";
-import metaTags from "astro-meta-tags";
 import react from "@astrojs/react";
-import qwikdev from "@qwikdev/astro";
-import jopSoftwarecookieconsent from "@jop-software/astro-cookieconsent";
 
 // https://astro.build/config
 export default defineConfig({
