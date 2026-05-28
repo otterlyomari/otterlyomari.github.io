2fdace781902719776945360c00b23fd17646a43
neon-gr33n <161001270+otterlyomari@users.noreply.github.com>
2026-05-28 02:47:26 -0400

    Update config for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://otterlyomari.github.io',
 
   // IMPORTANT for Cloudflare Pages (static hosting)
-  output: 'static',
+  output: 'server',
+  adapter: cloudflare(),
 
   // no base needed unless you're deploying under a subpath
   base: '/',
