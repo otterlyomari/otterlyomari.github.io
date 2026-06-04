6df803eb1d5a045460549f499b8cbcabf957affc
neon-gr33n <161001270+otterlyomari@users.noreply.github.com>
2026-06-04 16:01:07 -0400

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,10 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://otterlyomari.com',
-  output: 'server',
-  adapter: cloudflare({
-      platformProxy: {
-      enabled: false,
-    },
-    imageService: 'passthrough',
-  }),
+  output: 'static',
   
   base: '/',
 
